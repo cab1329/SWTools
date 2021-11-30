190ba362c7529f1efda591bdad856ff9a8983d1b
BradWiersema <71853382+BradWiersema@users.noreply.github.com>
2021-11-30 10:38:08 -0600

    Add files via upload

---
+++ StorefrontClass.py
@@ -0,0 +1,29 @@
+class Storefront:
+    
+    def __init__(self, inventory, users):
+        self.inventory = inventory
+        self.users = users
+
+    def __displayMenu(self):
+        choice = True
+        while choice == True:
+            print("1. Check Out\n2. Check Inventory\n3. Add item to cart\n")
+            print("4. Add item to Cart\n5. Remove Item from cart\n6. Log Out\n")
+            
+    def __checkOut(self, """whatever the cart object is called"""):
+
+    def __loadInventory(self):
+
+    def __checkInventory(self):
+
+    def __loadUsers(self):
+
+    def __loadOrders(self):
+
+    def __returnInformation(self):
+
+    def __writeOrders(self):
+
+    def __writeCart(self):
+
+    def __writeItem(self):
