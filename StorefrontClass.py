b5d76d57fdd3ddd24c3244ef8f28a20cda63f188
BradWiersema <71853382+BradWiersema@users.noreply.github.com>
2021-12-01 09:59:08 -0600

    Add files via upload

--- StorefrontClass.py
+++ StorefrontClass.py
@@ -7,10 +7,12 @@ from shopping_cart import Shopping_Cart
 
 class Storefront:
     
-    def __init__(self, inventory, users, orders):
+    def __init__(self, inventory, users, orders, cart, currentUser):
         self.inventory = inventory
         self.users = users
         self.orders = orders
+        self.cart = cart
+        self.currentUser = currentUser
 
     def displayMenu(self):
         choice = True
@@ -24,17 +26,25 @@ class Storefront:
                 self.checkInventory()
             else if num == 2:
                 print("1. View Cart\n2.Remove item from cart\n3. Add item to cart\n")
-                print("4. Add item to cart\n5. Empty cart\n6. Check out\n7. Go back\n")
+                print("4. Empty cart\n5. Check out\n6. Go back\n")
 
                 cartNum = int(input("Please enter the number of your choice: " ))
 
                 if cartNum == 1:
+                    self.cart.result_cart() 
                 else if cartNum == 2:
+                    print("Which item would you like to remove?\n")
+                    for x in self.cart:
+                        print(x, ". ", self.cart(x), "\n")
+
+#not sure how to finish this command off, open to suggestions                        
                 else if cartNum == 3:
+                    #need to know how many items we are going to have before i create this command, also not sure how to implement
                 else if cartNum == 4:
+                    self.cart.clearCart()
                 else if cartNum == 5:
+                    #again not sure how to implement
                 else if cartNum == 6:
-                else if cartNum == 7:
                     pass
                 else:
                     print("Invalid answer, returning to main menu")
@@ -45,7 +55,10 @@ class Storefront:
                 accountNum = int(input("Please enter the number of your choice: "))
 
                 if accountNum == 1:
+                    currentUser#display function
                 else if accountNum == 2:
+                    for x in orders:
+                        #displaying orders, figure out later 
                 else if accountNum == 3:
                     print("1. Edit shipping address\n2. Edit username\n3. Edit password\n")
                     print("4. Edit first name\n5. Edit last name\n6. Edit phone number\n7. Go Back\n")
@@ -53,11 +66,23 @@ class Storefront:
                     editNum == int(input("Please enter the number of your choice: "))
 
                     if editNum == 1:
+                        editAddress = input("Enter your new address: ")
+                        self.currentUser.setAddress(editAddress)
                     else if editNum == 2:
+                        editUsername = input("Enter your new username: ")
+                        self.currentUser.setUsername(editUsername)
                     else if editNum == 3:
+                        editPassword = input("Enter your new password: ")
+                        self.currentUser.setPassword(editPassword)
                     else if editNum == 4:
+                        editFirst = input("Enter your new first name: ")
+                        self.currentUser.setFirstname(editFirst)
                     else if editNum == 5:
+                        editLast = input("Enter your new last name: ")
+                        self.currentUser.setLastname(editLast)
                     else if editNum == 6:
+                        editNumber = input("Enter your new phone number: ")
+                        self.currentUser.setPhone(editNumber)
                     else if editNum == 7:
                         pass
                     else:
